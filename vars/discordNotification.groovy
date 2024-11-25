e37b40ab6aedafc2c5b64370f0c4fc8a51345458
Remi Bodin <remi.bodin@gmail.com>
2024-11-25 13:13:24 +0100

    Add discord notification method

---
+++ vars/discordNotification.groovy
@@ -0,0 +1,17 @@
+def call(String webhookURL, String imageURL) {
+
+    def description = ""
+    description += "* **Id**: #" + currentBuild.id + "\n"
+    description += "* **Branch**: " + env.PLASTICSCM_BRANCH + "\n"
+    description += "* **Builder**: " + env.NODE_NAME + "\n"
+    description += "* **Time**: " + currentBuild.durationString + "\n"
+    
+    discordSend customUsername:"CI", 
+        customAvatarUrl:"https://img.freepik.com/premium-photo/cute-smiling-robot_553012-14121.jpg", 
+        title: currentBuild.projectName, 
+        result: currentBuild.currentResult, 
+        description: description,
+        link: currentBuild.absoluteUrl, 
+        image: imageURL,
+        webhookURL:webhookURL
+}
