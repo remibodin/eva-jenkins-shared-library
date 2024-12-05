7cf48179a2408b8e87d54515c47b6a10826b2cf3
Remi Bodin <remi.bodin@gmail.com>
2024-12-05 16:34:41 +0100

    Fix unityEditorPath error with space

--- vars/unityCommandLine.groovy
+++ vars/unityCommandLine.groovy
@@ -1,5 +1,5 @@
 def call() {
-    def unityEditorPath = "$UNITY_EDITORS_FOLDER\\$UNITY_VERSION\\Editor\\Unity.exe"
+    def unityEditorPath = "\"$UNITY_EDITORS_FOLDER\\$UNITY_VERSION\\Editor\\Unity.exe\""
     def unityOptions = "-batchmode -quit -logFile -"
     def unityProject = "-projectPath \"$WORKSPACE\""
 
