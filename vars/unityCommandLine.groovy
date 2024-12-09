6575b929193d3c33f42f5c96a13aa8b69c713712
Remi Bodin <remi.bodin@gmail.com>
2024-12-09 14:49:03 +0100

    Update unity command line
    enable cache server only if UNITY_ACCELERATOR_IP *AND* UNITY_CACHE_PREFIX are set

--- vars/unityCommandLine.groovy
+++ vars/unityCommandLine.groovy
@@ -1,11 +1,13 @@
 def call() {
     def unityEditorPath = "\"$UNITY_EDITORS_FOLDER\\$UNITY_VERSION\\Editor\\Unity.exe\""
     def unityOptions = "-batchmode -quit -logFile -"
-    def unityProject = "-projectPath \"$WORKSPACE\""
+    def unityProjectPath = "-projectPath \"$WORKSPACE\""
 
-    if (env.UNITY_ACCELERATOR_IP != null) {
+    if (env.UNITY_ACCELERATOR_IP != null &&
+        env.UNITY_CACHE_PREFIX != null) 
+    {
         unityOptions += " -EnableCacheServer -cacheServerEndpoint \"$UNITY_ACCELERATOR_IP\" -cacheServerNamespacePrefix $UNITY_CACHE_PREFIX -cacheServerEnableDownload true -cacheServerEnableUpload true"
     }
 
-    return "${unityEditorPath} ${unityOptions} ${unityProject}"
+    return "${unityEditorPath} ${unityOptions} ${unityProjectPath}"
 }
